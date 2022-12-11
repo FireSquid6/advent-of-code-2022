b5e43e97e0e1558250818bd93ee49e1586f1a4cd
firesquid6 <jdeiss06@gmail.com>
2022-12-10 22:08:33 -0600

    finish day 10

--- day-10/part-1.py
+++ day-10/part-1.py
@@ -1,29 +1,51 @@
+correct_answer = "##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......###########.......#######.......#######....."
 
-
-def cycle(amount):
-    pass
-
-
-with open("./test-input.txt") as file:
-    cycle = 0
-    cooldown = 20
+class CPU:
+    cycles = 0
     register = 1
-    total_strength = 0
-    
+    strengths = []
+    crt = [""]
+    crt_row = 0
+    draw_index = 0
+
+    def cycle(self, amount):
+        # START OF CYCLE
+        self.cycles += 1
+
+        # DURING CYCLE
+        pixel = "."
+        if self.draw_index >= self.register - 1 and self.draw_index <= self.register + 1:
+            pixel = "#"
+
+        self.crt[self.crt_row] += pixel
+        if len(self.crt[self.crt_row]) == 40:
+            self.crt_row += 1
+            self.crt.append("")
+            self.draw_index = -1
+
+        
+        self.draw_index += 1
+        
+        # END OF CYCLE
+        self.register += amount
+        
+
+
+with open("./input.txt") as file:
+    cpu = CPU()
+
     for i, line in enumerate(file):
         line = line.strip()
-
-        if "noop" in line:
-            cycle += 1
-        elif "addx" in line:
-            cycle += 2
-            number = int(line.split(" ")[1])
-            register += number
-            
-            print(number)
+        cpu.cycle(0)
         
-        if cooldown == 0:
+        if "addx" in line:
+            number = int(line.split(" ")[1])
+            cpu.cycle(number)
 
-            cooldown = 40
-
-        cooldown -= 1
+    print(cpu.cycles)
+    merged = ""
+    for crt_row in cpu.crt:
+        print(crt_row)
+        merged += crt_row
+    
+    print(merged)
